eda03e6c9d6a0ff7243ddf2ff09528c66ffd2586
OlgaYaritskaya <olga.yaritskaya@yandex.ru>
2024-03-31 17:39:44 +0300

    Add project files.

---
+++ FakeRestApi/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FakeRestApi
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
