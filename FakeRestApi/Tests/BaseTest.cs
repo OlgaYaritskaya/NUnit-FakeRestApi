1e2d1e117da596571e736bca4b60cda934aabf4e
OlgaYaritskaya <olga.yaritskaya@yandex.ru>
2024-03-31 20:28:06 +0300

    add BaseTest with SetUp attribute

---
+++ FakeRestApi/Tests/BaseTest.cs
@@ -0,0 +1,31 @@
+﻿using FakeRestApi.Requests;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace FakeRestApi.Tests
+{
+    public class BaseTest
+    {
+        public RestClient client;
+        public RequestClass requestClass;
+        private string _baseUrl = "https://fakerestapi.azurewebsites.net";
+
+        [SetUp]
+        public void SetUp()
+        {
+            //client = new RestClient(_baseUrl);
+            requestClass = new RequestClass();
+        }
+
+        //[TearDown]
+        //public void TearDown()
+        //{
+        //    client.Dispose();
+        //}
+    }
+}
